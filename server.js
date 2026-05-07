5b6259b4b83b5aa527a69eb46758093bb2efea58
saef60500-lab <saef60500@gmail.com>
2026-05-08 00:30:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,17 @@ app.use(express.json());
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox']
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     }
 });
 
